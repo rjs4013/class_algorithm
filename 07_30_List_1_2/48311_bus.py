8ef10882a5d72c673cb0195d4e8411ffb71c2081
rjs4013 <rjs401323@gamil.com>
2024-08-09 09:05:14 +0900

    0809

--- 07_30_List_1_2/48311_bus.py
+++ 07_30_List_1_2/48311_bus.py
@@ -1,18 +1,23 @@
 T = int(input())
-for test_case in range(1,T+1):
+for test_case in range(1, T + 1):
     K, N, M = map(int, input().split())
     bus_number = list(map(int, input().split())) + [N]
+    a = 0
     i = 0
     count = 0
-    while i+K < N:
-        for j in range(len(bus_number)-1):
-            if i+K == bus_number[j]:
+    while i + K < N:
+        if a == 1:
+            break
+        for j in range(len(bus_number) - 1):
+            if i + K == bus_number[j]:
                 i = bus_number[j]
                 count += 1
-            elif i+K > bus_number[j] and i+K < bus_number[j+1]:
+            elif i + K > bus_number[j] and i + K < bus_number[j + 1]:
                 i = bus_number[j]
                 count += 1
-            elif i+K < bus_number[j]:
+            elif i + K < bus_number[j] and i + K < bus_number[j + 1]:
+                a = 1
+                count = 0
+                break
 
-
-    print(count)
+    print(f'#{test_case} {count}')
