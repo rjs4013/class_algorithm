187228330b3ad34c129853b73bacda078ce89b7f
rjs4013 <rjs401323@gmail.com>
2024-08-16 17:07:11 +0900

    [Bronze I] Title: 색종이, Time: 1448 ms, Memory: 119092 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/10163. 색종이/색종이.py
+++ 백준/Bronze/10163. 색종이/색종이.py
@@ -6,7 +6,7 @@
 
 N = int(input())
 paper = [list(map(int, input().split())) for _ in range(N)]
-
+# 색종이가 칠해지는 판 만들기 -> 첫번째 색종이는 1로, 두번째 색종이는 2로 ---- -> 겹쳐지는건 계속 숫자가 업데이트 되게 만듬
 pan = [[0] * 1001 for _ in range(1001)]
 
 for i in range(N):
@@ -14,17 +14,9 @@ for i in range(N):
         for k in range(paper[i][3]):
             pan[paper[i][0]+j][paper[i][1]+k] = i+1
 
-count = 0
-
-for x in range(1001):
-    for y in range(1001):
-        if pan[x][y] >= 2:
-            count += 1
-
 for q in range(N):
     re_count = 0
-    if q == N-1:
-        re_count = paper[q][2] * paper[q][3]
+    if q == N-1: re_count = paper[q][2] * paper[q][3] # 마지막 색종이는 전체 다 보이니까 넓이 그대로 출력
     else:
         for i in range(paper[q][2]):
             for j in range(paper[q][3]):
